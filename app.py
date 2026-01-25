3ea774f44cdd3ecbfb988214f263058f43962d76
CorentinJay <jay.corentin@hotmail.fr>
2026-01-25 11:59:23 +0100

    test

--- app.py
+++ app.py
@@ -131,15 +131,20 @@ def format_game_display(row):
 def get_today_games():
     try:
         df_schedule = pd.read_parquet('season_schedule.parquet')
-        df_schedule['Date'] = pd.to_datetime(df_schedule['Date'])
-        df_schedule['Heure_dt'] = pd.to_datetime(df_schedule['Heure'])
+        df_schedule['Date'] = pd.to_datetime(df_schedule['Date'], format='%d/%m/%Y')
+        
+        heure_time = pd.to_datetime(df_schedule['Heure'])
+        df_schedule['Heure_complete'] = pd.to_datetime(
+            df_schedule['Date'].dt.strftime('%Y-%m-%d') + ' ' + 
+            heure_time.dt.strftime('%H:%M:%S')
+        )
         
         paris_tz = pytz.timezone('Europe/Paris')
         
-        if df_schedule['Heure_dt'].dt.tz is None:
-            df_schedule['Heure_paris'] = df_schedule['Heure_dt'].dt.tz_localize('UTC').dt.tz_convert(paris_tz)
+        if df_schedule['Heure_complete'].dt.tz is None:
+            df_schedule['Heure_paris'] = df_schedule['Heure_complete'].dt.tz_localize('UTC').dt.tz_convert(paris_tz)
         else:
-            df_schedule['Heure_paris'] = df_schedule['Heure_dt'].dt.tz_convert(paris_tz)
+            df_schedule['Heure_paris'] = df_schedule['Heure_complete'].dt.tz_convert(paris_tz)
         
         now_paris = datetime.now(paris_tz)
         today_paris = now_paris.date()
