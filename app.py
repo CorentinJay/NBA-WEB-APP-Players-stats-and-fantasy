6682e92e18ff3901c87b5aa472a8a4a8c772a61a
CorentinJay <jay.corentin@hotmail.fr>
2026-04-02 09:31:51 +0200

    Commit : fix today games (date format) - test v3

--- app.py
+++ app.py
@@ -183,16 +183,13 @@ def get_today_games():
             return pd.DataFrame()
         
         df_schedule = load_season_schedule()
-        # df_schedule['Date'] = pd.to_datetime(df_schedule['Date'], format='mixed', dayfirst=True)
-        def safe_parse_date(d):
-            for fmt in ['%d/%m/%Y', '%m/%d/%Y', '%Y-%m-%d']:
-                try:
-                    return pd.to_datetime(d, format=fmt)
-                except:
-                    continue
-            return pd.NaT
+        if df_schedule['Date'].dtype == 'object':
+            df_schedule['Date'] = pd.to_datetime(df_schedule['Date'], format='mixed', dayfirst=True)
+        else:
+            df_schedule['Date'] = pd.to_datetime(df_schedule['Date'])
 
-        df_schedule['Date'] = df_schedule['Date'].apply(safe_parse_date)
+        st.write(df_schedule['Date'].dtype)
+        st.write(df_schedule['Date'].head(5))
         
         def parse_et_time(statut, date):
             try:
